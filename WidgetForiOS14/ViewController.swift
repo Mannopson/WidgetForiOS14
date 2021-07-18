379ff8ea75fb542a75490f7b586d75c986abb8cd
Mannopson <mannopson@yahoo.com>
2021-07-18 09:53:31 +0500

    Initial Commit

---
+++ WidgetForiOS14/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WidgetForiOS14
+//
+//  Created by AzizOfficial on 7/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
